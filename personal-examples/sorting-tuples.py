387cf12cd32cbeb57b612f8da10dbbba0323c73f
Dex <adderleydex@gmail.com>
2024-11-12 16:16:48 -0500

    sorting tuples examples

---
+++ personal-examples/sorting-tuples.py
@@ -0,0 +1,9 @@
+### Problem 2: Sorting Tuples by the Last Element
+
+# You have a list of tuples, where each tuple contains two elements. Write a lambda function that sorts this list in ascending order based on the second element of each tuple.
+
+# **Instructions:**
+
+# 1. Use the `sort` method with a lambda function as the key to access the last element of each tuple.
+# 2. Print the sorted list.
+
